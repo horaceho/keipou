76bd6ae4c20f7710cd2034a82102f36770ff721f
Horace Ho <horaceh@gmail.com>
2021-07-07 10:52:25 +0800

    install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,12 @@
   <Hello msg="棋譜・Keipou" />
 </template>
 
-<script setup>
+<script>
 import Hello from './components/Hello.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default {
+  components: {
+    Hello,
+  },
 }
-</style>
+</script>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,19 +1,32 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="p-4 border-b border-gray-100">
+    <div class="-ml-2 -mt-2 flex flex-wrap items-baseline">
+      <h3 class="ml-2 mt-2 text-lg leading-6 font-medium text-gray-900">
+        {{ msg }}
+      </h3>
+      <p class="ml-2 mt-1 text-sm text-gray-500 truncate">© 2021</p>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { defineProps, reactive } from 'vue'
+<script>
+import { reactive } from "vue";
 
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
+export default {
+  props: {
+    msg: String,
+  },
+  mounted() {
+    console.log("hello mounted()");
+  },
+  unmounted() {
+    console.log("hello unmounted()");
+  },
+  setup(props) {
+    const state = reactive({ count: 0 });
+    return {
+      state,
+    };
+  },
+};
 </script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
