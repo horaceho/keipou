9d63ebf890585c6ce72f1f109e0b789fa979bd00
Horace Ho <horaceh@gmail.com>
2021-07-08 20:23:38 +0800

    add icons in pous view

--- src/views/Pous.vue
+++ src/views/Pous.vue
@@ -3,14 +3,29 @@
     <h3 class="text-lg leading-6 font-medium text-gray-900">棋譜</h3>
   </div>
   <ul class="py-2 px-4 sm:px-6">
-    <li v-for="item in data">{{ item.name }} {{ item.type }}</li>
+    <li v-for="item in data">
+      <div class="flex divide-x divide-gray-200">
+        <div class="w-0 flex-1 flex">
+          <a :href="`/`" class="relative w-0 flex-1 inline-flex py-2 text-sm text-gray-700 font-medium border border-transparent">
+            <FolderIcon v-if="item.type === 'dir'" class="w-5 h-5 text-gray-600" aria-hidden="true" />
+            <DocumentIcon v-else class="w-5 h-5 text-gray-600" aria-hidden="true" />
+            <span class="ml-2">{{ item.name }}</span>
+          </a>
+        </div>
+    </div>
+    </li>
   </ul>
 </template>
 
 <script>
-const url = "https://api.github.com/repos/yaumati/keipou/contents/public/棋譜/";
+import { FolderIcon, DocumentIcon } from "@heroicons/vue/solid";
+const url = "https://api.github.com/repos/yaumati/keipou/contents/public/棋譜";
 
 export default {
+  components: {
+    FolderIcon,
+    DocumentIcon,
+  },
   data() {
     return {
       data: {},
