d2052799bdde793042780c68578fb1fa2bb350cd
Horace Ho <horaceh@gmail.com>
2021-07-10 12:20:35 +0800

    update board svg

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="">
     <Board fen="rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1" class="mt-8" />
+    <Board fen="9/9/9/9/9/9/9/9/9/9 r - - 0 1" class="mt-8" />
     <Board fen="rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/9/1C5C1/9/RN2K2NR r - - 0 1" class="mt-8" />
     <Board fen="2baka3/3P3N1/bN7/7nc/9/4C1P2/P5n1P/B3R3B/4Apr2/2RAK3c r - - 0 1" class="mt-8" />
     <Board fen="4k4/9/3aP3b/p8/9/6n2/2P3p2/4R4/3p1p3/4K4 r - - 0 1" class="mt-8" />
