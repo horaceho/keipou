d68be4c99d66ec35c0b682e04289e3e5bfc06f76
Horace Ho <horaceh@gmail.com>
2021-07-12 18:29:15 +0800

    add game view

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,22 +1,91 @@
 <template>
-  <div class="grid grid-cols-9 mx-auto m-5 w-72 h-80 bg-board bg-no-repeat bg-yellow-100">
-    <div v-for="grid in grids" :key="grid.index">
-      <div class="w-8">
-        <Piece :grid="grid" @onTapped="onTapped" />
+  <div class="flex">
+    <div class="flex-auto m-5">
+      <div class="grid grid-cols-1 place-items-end">
+        <div v-for="grid in headBox" :key="grid.index">
+          <div class="w-8">
+            <Piece :grid="grid" @onTapped="onHeadBoxTapped" />
+          </div>
+        </div>
       </div>
     </div>
+    <div>
+      <div class="grid grid-cols-9 mx-auto m-5 w-72 h-80 bg-board bg-no-repeat">
+        <div v-for="grid in grids" :key="grid.index">
+          <div class="w-8">
+            <Piece :grid="grid" @onTapped="onPieceTapped" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex-auto m-5">
+      <div class="grid grid-cols-1 place-items-start">
+        <div v-for="grid in tailBox" :key="grid.index">
+          <div class="w-8">
+            <Piece :grid="grid" @onTapped="onTailBoxTapped" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-if="mode === 'play'">
+    <Float :icons="playIcons" @onTapped="onFloatTapped" />
+  </div>
+  <div v-if="mode === 'edit'">
+    <Float :icons="editIcons" @onTapped="onFloatTapped" />
   </div>
 </template>
 
 <script>
 import Piece from "../components/Piece.vue";
+import Float from '../components/Float.vue'
+
+const Icons = {
+  play: [
+    {
+      name: "✎",
+      code: "edit",
+    },
+    {
+      name: "◁",
+      code: "back",
+    },
+    {
+      name: "▶",
+      code: "next",
+    },
+    {
+      name: "⌂",
+      code: "home",
+    },
+  ],
+  edit: [
+    {
+      name: "✓",
+      code: "play",
+    },
+    {
+      name: "⌂",
+      code: "home",
+    },
+  ],
+};
 
 export default {
   components: {
     Piece,
+    Float,
+  },
+  computed: {
+    playIcons() {
+      return Icons['play'];
+    },
+    editIcons() {
+      return Icons['edit'];
+    },
   },
   methods: {
-    onTapped(event) {
+    onPieceTapped(event) {
       if (this.grids[event.index].tapped) {
         this.grids[event.index].tapped = false;
         let position = this.taps.indexOf(event.index);
@@ -34,6 +103,36 @@ export default {
           this.grids[this.taps[0]].code = "1";
         }
         this.clearTaps();
+      }
+
+      // if (this.mode === "idle") {
+      //   this.mode = "play";
+      // }
+    },
+    onHeadBoxTapped(event) {
+      if (event.index < 7) {
+        this.headBox[event.index].tapped = this.headBox[event.index].tapped ? false : true;
+      }
+    },
+    onTailBoxTapped(event) {
+      if (event.index > 2) {
+        this.tailBox[event.index].tapped = this.tailBox[event.index].tapped ? false : true;
+      }
+    },
+    onFloatTapped(event) {
+      switch (event.icon.code) {
+        case "edit":
+          this.mode = "edit";
+          break;
+        case "play":
+          this.mode = "play";
+          break;
+        case "back":
+          break;
+        case "next":
+          break;
+        case "home":
+          break;
       }
     },
     clearTaps() {
@@ -72,13 +171,39 @@ export default {
       }
       return grids;
     },
+    strToBox(str) {
+      let grids = [];
+      let chars = str.split("");
+      for (let one of chars) {
+        if (one.match(/\d/)) {
+          for (let i = 0; i < Number(one); i++) {
+            grids.push({
+              index: grids.length,
+              code: "1",
+              tapped: false,
+            });
+          }
+        } else {
+          grids.push({
+            index: grids.length,
+            code: one,
+            tapped: false,
+          });
+        }
+      }
+      return grids;
+    }
   },
   props: {
     fen: { type: String, default: "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/9/1C5C1/9/RN2K2NR r - - 0 1" },
+    init: { type: String, default: "null" },
   },
   data() {
     return {
       grids: [],
+      headBox: [],
+      tailBox: [],
+      mode: "idle", // play, edit
       turn: "red",
       move: 0,
       taps: [],
@@ -86,6 +211,9 @@ export default {
   },
   mounted() {
     this.grids = this.fenToGrids(this.fen);
+    this.headBox = this.strToBox("krncabp3");
+    this.tailBox = this.strToBox("3PBACNRK");
+    this.mode = this.init; // null, idle
     console.log("board mounted()");
   },
   unmounted() {
---
+++ src/views/Game.vue
@@ -0,0 +1,13 @@
+<template>
+  <Board fen="rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r - - 0 1" init="idle" />
+</template>
+
+<script>
+import Board from './Board.vue'
+
+export default {
+  components: {
+    Board,
+  },
+}
+</script>
