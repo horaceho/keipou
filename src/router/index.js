1e8b09a0842107ad706a8ef7badfde41c5f3d7cd
Horace Ho <horaceh@gmail.com>
2021-07-13 19:37:27 +0800

    add props name to home view

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    props: route => ({ name: route.query.name }),
   },
   {
     path: "/game",
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
     <h3 class="text-lg leading-6 font-medium text-gray-900">
-      Home
+      {{ name }}
     </h3>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    name: { type: String, default: "Home" },
+  },
   mounted() {
   },
   unmounted() {
