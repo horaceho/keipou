e000c066ec895f4a2967cbc793d070368b67bb20
Horace Ho <horaceh@gmail.com>
2021-07-17 15:58:07 +0800

    add board notation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       backgroundImage: theme => ({
         'xiang': "url('/src/assets/xiang.svg')",
         'board': "url('/src/assets/board.svg')",
+        'notion-b': "url('/src/assets/notion-b.svg')",
+        'notion-r': "url('/src/assets/notion-r.svg')",
        })
     },
   },
